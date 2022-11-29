cc8055893a9d16c680f0cff5d7e54a56d63aeabe
Ravi M <ravm2@publicisgroupe.net>
2022-11-29 23:49:21 +0530

    first commit

---
+++ src/test/java/assignment/Concatinate.java
@@ -0,0 +1,15 @@
+package assignment;
+
+import org.testng.annotations.Test;
+
+public class Concatinate {
+
+        @Test
+		public void name() {
+	     String a="Rav";
+		 String b="Mad";
+		 System.out.println("Hello"+" "+a+" "+b);
+
+	}
+
+}
